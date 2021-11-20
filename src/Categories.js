327312f96ced7245b0cefa3de89597f5f3b7aa67
Tursunjon-dev <tursunjon999@gmail.com>
2021-11-20 10:12:20 -0800

    the bests

---
+++ src/Categories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.css';
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            classNam="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
